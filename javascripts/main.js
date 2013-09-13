4d39be2fcbb3356947c412fa461147acfc8c1f05
Ben Kim <benjskim@gmail.com>
2013-09-12 17:22:45 -0700

    Day 4: Logo Movement
    Still not fluid enough

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,26 +1,26 @@
 // ND Logo Transition Effect
 
 function transitionIn() {
-  $(this).transition({
-    y: '-30px',
-    scale: .75
+  $('.nd').transition({
+    y: '-5px',
+    scale: .85
   });
+  $('.text7').fadeIn(1000);
 }
 
 function transitionOut() {
-  $(this).transition({
+  $('.nd').transition({
     y: '0px',
     scale: 1
-  });
+  });  
+  $('.text7').fadeOut(500);
 }
-
 
 $('.box-7').hoverIntent({ 
   over: transitionIn,
   timeout: 200,
   interval: 200,
-  out: transitionOut,
-  selector:'img'
+  out: transitionOut
 });
 
 // End ND Logo Transition
