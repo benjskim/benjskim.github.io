301c8273b5d7b7b51cfe374a2dad35f2d7ba03a3
Ben Kim <benjskim@gmail.com>
2013-09-18 22:08:24 -0700

    Day 10 Final

--- javascripts/main.js
+++ javascripts/main.js
@@ -70,11 +70,6 @@ $('.box-7').hoverIntent({
   // out: transitionOut
 });
 
-//---Imageslider---//
-$(document).ready(function() {
-  $('#slider').leanSlider();
-})
-
 //Menu Modal
 $('.modalabout').click(function() {
   $('.simple-modal').transition({
@@ -118,51 +113,3 @@ $('.closebutton').click(function() {
     duration: 300
   }).fadeOut(500);
 });  
-
-//---Firefox super responsive scroll (c) Keith Clark - MIT Licensed---//
-
-$(document).ready(function(doc) {
-
-  var root = doc.documentElement;
-
-  // Not ideal, but better than UA sniffing.
-  if ("MozAppearance" in root.style) {
-
-    // determine the vertical scrollbar width
-    var scrollbarWidth = root.clientWidth;
-    root.style.overflow = "scroll";
-    scrollbarWidth -= root.clientWidth;
-    root.style.overflow = "";
-    
-    // create a synthetic scroll event
-    var scrollEvent = doc.createEvent("UIEvent")
-    scrollEvent.initEvent("scroll", true, true);
-    
-    // event dispatcher
-    function scrollHandler() {
-      doc.dispatchEvent(scrollEvent)
-    }
-
-    // detect mouse events in the document scrollbar track
-    doc.addEventListener("mousedown", function(e) {
-      if (e.clientX > root.clientWidth - scrollbarWidth) {
-        doc.addEventListener("mousemove", scrollHandler, false);
-        doc.addEventListener("mouseup", function() {
-          doc.removeEventListener("mouseup", arguments.callee, false);
-          doc.removeEventListener("mousemove", scrollHandler, false);
-        }, false)
-      }
-    }, false)
-
-    // override mouse wheel behaviour.
-    doc.addEventListener("DOMMouseScroll", function(e) {
-      // Don't disable hot key behaviours
-      if (!e.ctrlKey && !e.shiftKey) {
-        root.scrollTop += e.detail * 16;
-        scrollHandler.call(this, e);
-        e.preventDefault()
-      }
-    }, false)
- 
-  }
-}); 
