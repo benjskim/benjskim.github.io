946802f9b44ef8fcae2e9819524fd36a570370e2
Ben Kim <benjskim@gmail.com>
2013-09-23 09:54:10 -0700

    Day 15: Full Page Scroll

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,11 @@
-$(document).ready(function() {
+$(function() {
   $(".fadeheader").transition({
     x: '30px',
     opacity: 1,
     duration: 1200,
-    delay: 1500
+    delay: 1350
   })
+
   $(".char1").transition({
     y: '30px',
     opacity: 1,
@@ -12,5 +13,11 @@ $(document).ready(function() {
     delay: 500
   })
 
-  $('nav').fadeIn();
+  $('#container').fullContent({ 
+    stages: 'div', 
+    mapPosition: [{v: 1, h: 1}, {v: 2, h: 1}, {v: 3, h: 1}, {v: 4, h: 1}], 
+    stageStart: 1, 
+    speedTransition: 800, 
+    idComplement: 'page_' 
+  });
 });
