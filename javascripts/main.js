76f99f4d8980562c6cb5a2a705ea550c800b1687
Ben Kim <benjskim@gmail.com>
2013-10-02 23:30:10 -0700

    Day 23 Final

--- javascripts/main.js
+++ javascripts/main.js
@@ -82,7 +82,7 @@ $(function() {
 
   $("img[alt='four']").on("mouseover", function() {
     $(this).transition({
-      rotate: '10deg',
+      rotate: '2deg',
       y: '-35',
       x: '-10'
     });
@@ -90,7 +90,7 @@ $(function() {
 
   $("img[alt='four']").on("mouseleave", function() {
     $(this).transition({
-      rotate: '15deg',
+      rotate: '10deg',
       y: '0',
       x: '0'
     });
@@ -99,7 +99,7 @@ $(function() {
   $("img[alt='five']").on("mouseover", function() {
     $(this).transition({
       rotate: '-15deg',
-      y: '-25',
+      y: '-15',
       x: '-20'
     });
   });
@@ -121,3 +121,4 @@ jQuery("nav").fitText(1.5, { minFontSize: '16px', maxFontSize: '16px' });
 jQuery("h1").fitText(1.2, { minFontSize: '60px', maxFontSize: '80px' });
 jQuery("h2").fitText(1.2, { minFontSize: '50px', maxFontSize: '64px' });
 jQuery("h3").fitText(1.5, { minFontSize: '30px', maxFontSize: '48px' });
+jQuery("p").fitText(1.5, { minFontSize: '20px', maxFontSize: '21px' });
