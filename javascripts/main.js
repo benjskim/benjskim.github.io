3a0e40b3ae1c362a4595bf9d031f1608424a7410
Ben Kim <benjskim@gmail.com>
2013-10-15 15:25:37 -0700

    Read More / Read Less JS

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,6 +8,13 @@ $(function() {
   });
 
   $(".moreabout").click(function(){
+    if ($(".moreabout").text() == "Read More") 
+      {
+        $(".moreabout").text("Read Less");
+      } else 
+        {
+          $(".moreabout").text("Read More");
+        }
     $(".moreabouttext").slideToggle();
   });
 });
