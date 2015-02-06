70f3936e7f02fd12ddafbf39c0aae614c9445ce3
Ben Kim <benjskim@gmail.com>
2015-02-05 19:27:14 -0800

    Part 25: FadeIn Interactions

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+  $(".headpic").css({ opacity: 1});
   $(".portfolio-card").css({ opacity: 1});
+  $(".about-bg-img").css({ opacity: 1});
   $(".fancybox").fancybox();
   $("#single_1").fancybox({
           helpers: {
